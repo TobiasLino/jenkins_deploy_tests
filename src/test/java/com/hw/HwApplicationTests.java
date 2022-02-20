5f411837338b42d0f6bcd6c4f89f22cc386d6adf
TobiasLino <linop019@gmail.com>
2022-02-20 13:28:03 -0300

    adding a single test

--- src/test/java/com/hw/HwApplicationTests.java
+++ src/test/java/com/hw/HwApplicationTests.java
@@ -3,11 +3,15 @@ package com.hw;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @SpringBootTest
 class HwApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat("Hello World", equalTo("Hello World"));
 	}
 
 }
